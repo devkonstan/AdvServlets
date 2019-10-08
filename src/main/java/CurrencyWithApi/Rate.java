95563c2045ba8f1b818c60d7f133383287ef9217
devkonstan <konrad.stankievich@gmail.com>
2019-10-09 01:20:05 +0200

    source files

---
+++ src/main/java/CurrencyWithApi/Rate.java
@@ -0,0 +1,15 @@
+package CurrencyWithApi;
+
+import java.util.Map;
+
+public class Rate {
+
+    public Map<String, Float> rates;
+
+    @Override
+    public String toString() {
+        return "Rate{" +
+                "rates=" + rates +
+                '}';
+    }
+}
